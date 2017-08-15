f029bfa3de9dc2b020ba78614548b77ee7776cc0
jongrover <hello@jonathangrover.com>
2017-08-15 16:26:40 -0400

    set accordion gallery images to hide initially

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,4 @@
-$(window).on('load', function () {
+$(document).ready(function () {
 
   var $accordion = $('.accordion'),
       $panel = $accordion.find('.panel'),
@@ -11,32 +11,40 @@ $(window).on('load', function () {
       imgWidth,
       originImgWidth = [];
 
-  $img.each(function(i,img) {
-    originImgWidth[i] = $(img).width();
-  });
+  $img.hide();
 
-  function setWidths() {
-    accordionWidth = $accordion.width();
+  $(window).on('load', function () {
+
+    $img.show();
+
     $img.each(function(i,img) {
-      imgWidth = (originImgWidth[i] * accordionWidth) / maxAccordionWidth;
-      $(img).width(imgWidth);
+      originImgWidth[i] = $(img).width();
     });
-  }
-  setWidths();
-  $(window).resize(setWidths);
 
-  $panel.css({'width': panelWidth+"%"});
+    function setWidths() {
+      accordionWidth = $accordion.width();
+      $img.each(function(i,img) {
+        imgWidth = (originImgWidth[i] * accordionWidth) / maxAccordionWidth;
+        $(img).width(imgWidth);
+      });
+    }
+    setWidths();
+    $(window).resize(setWidths);
 
-  $panel.click(function () {
-    var $selPanel = $(this),
-        $nonSelPanels = $panel.not($selPanel),
-        $selImg = $selPanel.find('img'),
-        $nonSelImg = $img.not($selImg),
-        selImgWidth = $selImg.width(),
-        selPanelPercent = ((selImgWidth / accordionWidth) * 100),
-        nonSelPanelsPercent = ((100 - selPanelPercent) / (panelCount - 1));
-    $nonSelPanels.removeClass('expanded').addClass('collapsed').animate({'width': nonSelPanelsPercent+"%"}, animateSpeed);
-    $selPanel.removeClass('collapsed').addClass('expanded').animate({'width': selPanelPercent+"%"}, animateSpeed);
+    $panel.css({'width': panelWidth+"%"});
+
+    $panel.click(function () {
+      var $selPanel = $(this),
+          $nonSelPanels = $panel.not($selPanel),
+          $selImg = $selPanel.find('img'),
+          $nonSelImg = $img.not($selImg),
+          selImgWidth = $selImg.width(),
+          selPanelPercent = ((selImgWidth / accordionWidth) * 100),
+          nonSelPanelsPercent = ((100 - selPanelPercent) / (panelCount - 1));
+      $nonSelPanels.removeClass('expanded').addClass('collapsed').animate({'width': nonSelPanelsPercent+"%"}, animateSpeed);
+      $selPanel.removeClass('collapsed').addClass('expanded').animate({'width': selPanelPercent+"%"}, animateSpeed);
+    });
+
   });
 
 });
